0370669d89dbd560ca7d8cdb25075952948ccb7b
Anton Moiseev <antonmoiseev@Antons-MBP.Dlink>
2018-12-15 21:04:15 +0300

    enterWord.go

---
+++ functions/enterWord.go
@@ -0,0 +1,11 @@
+// Package keyboard reads user input from the keyboard.
+package keyboard
+
+import (
+	"fmt"
+)
+
+// Enter func allow user inter new word
+func Enter() {
+	fmt.Println("Enter new word: ")
+}
---
+++ hangman.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/reiwing/hangman/functions"
+)
+
+func main() {
+	enterWord.Enter()
+}
